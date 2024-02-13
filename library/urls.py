bdf2f2f46cc393bf504286526ad48e58e14472c8
AlexNdungu <alexmeta517@gmail.com>
2024-02-13 12:23:15 +0300

    Authentication

--- library/urls.py
+++ library/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
     # Sign up url
     path('signup/',views.signUp, name='sign_up'),
 
@@ -11,13 +12,23 @@ urlpatterns = [
     # Sign in url
     path('signin/',views.signIn, name='signin'),
 
-    # Sign out url
-    path('signout/',views.signOut, name='signout'),
-
     # Sign in url
     path('signinUser/', views.signInUser, name='signin_user'),
 
+    # Sign out url
+    path('signout/',views.signOut, name='signout'),
+
+    # Upper Navigation Inherit Url
+    path('upperNav/', views.UpperNav, name='upper-nav'),
+
     # landing Page Url
     path('', views.Landing, name='landing'),
+
+    # Profile Url
+    path('profile/', views.Profile, name='profile'),
+
+    # Update Profile Url
+    path('updateProfile/', views.UpdateProfile, name='update_profile'),
+
 
 ]
--- library/views.py
+++ library/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.contrib.auth import login,logout
+from django.contrib.auth.decorators import login_required
+from django.core.files.storage import default_storage
+from . import models
+
 
 
 # Sign up page
@@ -12,8 +16,9 @@ def signUp(request):
         return redirect('dash')
     else:
         return render(request,'Main/signup.html', {'error':''})
-    
-    # Create new user
+
+   
+# Create new user
 def createNewUser(request):
 
     #Email will always be unique for any user
@@ -89,7 +94,11 @@ def signInUser(request):
         else:
 
             return JsonResponse({'status':'not_found'})
-        
+
+# The Inherited upper Navigation Rendering Function
+def UpperNav(request):
+    return render(request,'Inherit/upper-nav.html')
+
 # Landing Page
 def Landing(request):
 
@@ -98,3 +107,91 @@ def Landing(request):
         return redirect('dash')
     else:
         return render(request,'Main/landing.html')
+    
+
+# The profile rendering function
+@login_required
+def Profile(request):
+
+    # Get the profile picture url of current user
+    profile = models.Profile.objects.get(user = request.user)
+
+    if profile.profile_pic != '':
+        profile_pic_url = profile.profile_pic.url
+    else:
+        profile_pic_url = ''
+
+    data_dict = {
+        'profile_pic_url':profile_pic_url,
+    }
+
+    return render(request, 'Main/profile.html', data_dict)
+
+
+# Update Profile
+def UpdateProfile(request):
+    
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+
+        # Get the data
+        update_item = request.POST.get('to_update')
+        # Get the current profile
+        current_profile = models.Profile.objects.get(user = request.user)
+
+        # Updating profile picture
+        if update_item == 'profile_pic':
+                
+            # Get the profile picture
+            profile_pic = request.FILES.get('profile_pic')
+            # Update the profile picture
+            current_profile.profile_pic = profile_pic
+            current_profile.save()
+            profile_pic_url = current_profile.profile_pic.url
+
+            return JsonResponse({'profile_pic_url':profile_pic_url})
+        
+        elif update_item == 'remove_profile_pic':
+
+            # Delete the profile picture
+            default_storage.delete(current_profile.profile_pic.path)
+            current_profile.profile_pic = None
+            current_profile.save()
+
+            return JsonResponse({'message':'suceess'})
+        
+        elif update_item == 'bio':
+
+            # Get the bio
+            bio = request.POST.get('bio')
+            current_profile.bio = bio
+            current_profile.save()
+            # Get the new bio
+            bio = current_profile.bio
+
+            return JsonResponse({'bio':bio})
+        
+        elif update_item == 'all_details':
+
+            # Get the data : fullname,secondary_email,company,location,website
+            full_name = request.POST.get('fullname')
+            secondary_email = request.POST.get('secondary_email')
+            company = request.POST.get('company')
+            location = request.POST.get('location').capitalize()
+            website = request.POST.get('website')
+
+            # Update the data
+            current_profile.full_name = full_name
+            current_profile.secondary_email = secondary_email
+            current_profile.company = company
+            current_profile.location = location
+            current_profile.website = website
+            current_profile.save()
+
+            # Get the new data
+            new_full_name = current_profile.full_name
+            new_secondary_email = current_profile.secondary_email
+            new_company = current_profile.company
+            new_location = current_profile.location
+            new_website = current_profile.website
+
+            return JsonResponse({'fullname':new_full_name,'secondary_email':new_secondary_email,'company':new_company,'location':new_location,'website':new_website})
