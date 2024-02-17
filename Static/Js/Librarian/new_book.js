4911ced53a6ac767c1f3935af13d1f2669bb285a
AlexNdungu <alexmeta517@gmail.com>
2024-02-17 12:51:03 +0300

    Books

--- Static/Js/Librarian/new_book.js
+++ Static/Js/Librarian/new_book.js
@@ -83,7 +83,9 @@ function getAllCategories(){
                                 <span>${categories[i]}</span>
                             </div>
                             <div class="category_item_info" id='${new_category_id}'>
-                                <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12.002 2.005c5.518 0 9.998 4.48 9.998 9.997 0 5.518-4.48 9.998-9.998 9.998-5.517 0-9.997-4.48-9.997-9.998 0-5.517 4.48-9.997 9.997-9.997zm0 8c-.414 0-.75.336-.75.75v5.5c0 .414.336.75.75.75s.75-.336.75-.75v-5.5c0-.414-.336-.75-.75-.75zm-.002-3c-.552 0-1 .448-1 1s.448 1 1 1 1-.448 1-1-.448-1-1-1z" fill-rule="nonzero"/></svg>
+                                <div class="category_info_icon_section">
+                                    <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12.002 2.005c5.518 0 9.998 4.48 9.998 9.997 0 5.518-4.48 9.998-9.998 9.998-5.517 0-9.997-4.48-9.997-9.998 0-5.517 4.48-9.997 9.997-9.997zm0 8c-.414 0-.75.336-.75.75v5.5c0 .414.336.75.75.75s.75-.336.75-.75v-5.5c0-.414-.336-.75-.75-.75zm-.002-3c-.552 0-1 .448-1 1s.448 1 1 1 1-.448 1-1-.448-1-1-1z" fill-rule="nonzero"/></svg>
+                                </div>
                             </div>
                         </div>
                     `;
@@ -95,8 +97,16 @@ function getAllCategories(){
                 // Add event listiner to all category_item_info buttons
                 let category_item_info = document.getElementsByClassName('category_item_info');
                 for(let i = 0; i < category_item_info.length; i++){
-                    category_item_info[i].addEventListener('click', ()=> {
-                        showCategoryInfoPopup(category_item_info[i].id);
+                    category_item_info[i].addEventListener('click', (e)=> {
+
+                        // check if e.target has id and if it is not the category_item_info_popup_head
+                        if(e.target.id == '' && !e.target.classList.contains('category_item_info_popup_head')){
+                            showCategoryInfoPopup(category_item_info[i].id);
+                        }
+                        else if(e.target.classList.contains('category_item_info_popup_head')){
+                            return;
+                        }
+
                     });
                 }
 
@@ -128,7 +138,7 @@ function showCategoryInfoPopup(category_name){
 
     // replace - with space in category_name
     let new_category_name = category_name.replace(/-/g, ' ');
-    
+
     // remove the popup
     $('#category_item_info_popup').remove();
 
@@ -170,18 +180,19 @@ function showCategoryInfoPopup(category_name){
 
             // Get the close button and the popup
             let category_item_info_popup_close = document.getElementById('category_item_info_popup_close');
-            category_item_info_popup_close.addEventListener('click', ()=> {
-
-                // remove the popup
-                $('#category_item_info_popup').remove();
-
-                // show error message
-                message_popup_failed.style.display = 'flex';
-                failed_message_popup.innerHTML = 'Info popup closed.';
-                // hide the message
-                setTimeout(() => {
-                    message_popup_failed.style.display = 'none';
-                }, 4000);
+            category_item_info_popup_close.addEventListener('click', (e)=> {
+                
+                if(e.target.id != ''){
+                    // remove the popup
+                    $('#category_item_info_popup').remove();
+                    // show error message
+                    message_popup_failed.style.display = 'flex';
+                    failed_message_popup.innerHTML = 'Info popup closed.';
+                    // hide the message
+                    setTimeout(() => {
+                        message_popup_failed.style.display = 'none';
+                    }, 4000);
+                }
 
             });
 
