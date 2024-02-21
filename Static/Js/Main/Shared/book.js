8f95ed3cdf35276bae26ce68f513ed6b18a45c60
AlexNdungu <alexmeta517@gmail.com>
2024-02-21 07:44:25 +0300

    individual book

---
+++ Static/Js/Main/Shared/book.js
@@ -0,0 +1,13 @@
+// get all the needed items
+let csrf = document.getElementsByName('csrfmiddlewaretoken');
+//
+let message_popup_success = document.getElementById('message_popup_success');
+let success_message_popup = document.getElementById('success_message_popup');
+let message_popup_failed = document.getElementById('message_popup_failed');
+let failed_message_popup = document.getElementById('failed_message_popup');
+
+
+// functions
+function deleteBook(){
+    console.log(book_id);
+}
