af6923a3a2e11a80482906209cc51a25ec3ca42f
AlexNdungu <alexmeta517@gmail.com>
2024-02-23 08:33:42 +0300

    all users

---
+++ Static/Js/Librarian/all_users.js
@@ -0,0 +1,9 @@
+// get all items
+let csrf = document.getElementsByName('csrfmiddlewaretoken');
+let table_body = document.getElementById('table_body');
+let user_no_display = document.getElementById('user_no_display');
+//
+let message_popup_success = document.getElementById('message_popup_success');
+let success_message_popup = document.getElementById('success_message_popup');
+let message_popup_failed = document.getElementById('message_popup_failed');
+let failed_message_popup = document.getElementById('failed_message_popup');
