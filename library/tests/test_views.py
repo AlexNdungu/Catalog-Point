9abfcad9b377f4c6ddbe5083df06d2db3615ae08
AlexNdungu <alexmeta517@gmail.com>
2024-03-23 19:34:19 +0300

    Views Testing Process

--- library/tests/test_views.py
+++ library/tests/test_views.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from django.contrib.auth.models import Group
 from library.models import *
+import json
 
 class TestViews(TestCase):
 
@@ -24,6 +25,24 @@ class TestViews(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'Main/signup.html')
 
+    def test_create_new_user_existing_user(self):
+        client = Client()
+        create_test_user = User.objects.create_user(username='testuser@gmail.com', email='testuser@gmail.com', password='testpassword')
+        created_user_email = create_test_user.email
+        password = 'testpassword'
+        user_data = {'email': created_user_email, 'password': password}
+        response = client.post(reverse('create_new_user'), data=user_data, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEqual(response.json(), {'status':'exists'})
+
+    def test_create_user_new_exists_is_false(self):
+        client = Client()
+        email = 'test@gmail.com'
+        password = 'testpassword'
+        data = {'email':email, 'password':password}
+        response = client.post(reverse('create_new_user'),data=data, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEqual(response.json(), {'status':'created'})
+
+        
     def test_upper_nav_view(self):
         response = self.client.get(reverse('upper-nav'))
         self.assertEquals(response.status_code, 200)
