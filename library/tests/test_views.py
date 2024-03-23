1f61d4b2ee5b5a668b07b8249a7dfbb878c1e757
AlexNdungu <alexmeta517@gmail.com>
2024-03-23 19:48:01 +0300

    Views Testing Process

--- library/tests/test_views.py
+++ library/tests/test_views.py
@@ -5,14 +5,13 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from django.contrib.auth.models import Group
 from library.models import *
-import json
 
 class TestViews(TestCase):
 
     def setUp(self):
         self.librarian_group = Group.objects.create(name='librarian')
         self.member_group = Group.objects.create(name='member')
-        self.user = User.objects.create_user(username='testuser',password='testpassword')
+        self.user = User.objects.create_user(username='testuser',password='testpassword',email='testuser@gmail.com')
         self.client.force_login(self.user)
 
     def test_sign_up_view_logged_in(self):
@@ -27,21 +26,28 @@ class TestViews(TestCase):
 
     def test_create_new_user_existing_user(self):
         client = Client()
-        create_test_user = User.objects.create_user(username='testuser@gmail.com', email='testuser@gmail.com', password='testpassword')
-        created_user_email = create_test_user.email
-        password = 'testpassword'
-        user_data = {'email': created_user_email, 'password': password}
+        email = self.user.email
+        user_data = {'email': email}
         response = client.post(reverse('create_new_user'), data=user_data, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
         self.assertEqual(response.json(), {'status':'exists'})
 
     def test_create_user_new_exists_is_false(self):
         client = Client()
         email = 'test@gmail.com'
-        password = 'testpassword'
-        data = {'email':email, 'password':password}
+        #password = 'testpassword'
+        data = {'email':email}
         response = client.post(reverse('create_new_user'),data=data, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
         self.assertEqual(response.json(), {'status':'created'})
 
+    def test_sign_in_logged_in(self):
+        response = self.client.get(reverse('signin'))
+        self.assertEquals(response.status_code, 302)
+
+    def test_sign_in_logged_out(self):
+        client = Client()
+        response = client.get(reverse('signin'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'Main/signin.html')
         
     def test_upper_nav_view(self):
         response = self.client.get(reverse('upper-nav'))
