c5b6c6c3487d0cd15c3a45448ac894276396a465
AlexNdungu <alexmeta517@gmail.com>
2024-03-24 14:46:13 +0300

    Views Testing Process

--- library/tests/test_views.py
+++ library/tests/test_views.py
@@ -12,6 +12,7 @@ class TestViews(TestCase):
         self.librarian_group = Group.objects.create(name='librarian')
         self.member_group = Group.objects.create(name='member')
         self.user = User.objects.create_user(username='testuser',password='testpassword',email='testuser@gmail.com')
+        self.testImage = open('Static/Images/user.jpg', 'rb')
         self.client.force_login(self.user)
 
     def test_sign_up_view_logged_in(self):
@@ -98,7 +99,45 @@ class TestViews(TestCase):
         response = self.client.get(reverse('one_user', args=[self.user.username]))
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'librarian/user.html')
-        
+
+    def test_update_profile_add_and_remove_profilePicture(self):
+        # First add a profile picture
+        data = {'to_update':'profile_pic','profile_pic':self.testImage}
+        response = self.client.post(reverse('update_profile'), data=data, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEquals(response.status_code, 200)
+        # Then remove the profile picture
+        data1 = {'to_update':'remove_profile_pic'}
+        response1 = self.client.post(reverse('update_profile'), data=data1, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEquals(response1.status_code, 200)
+        self.assertEquals(response1.json(), {'message':'suceess'})
+
+    def test_update_profile_bio(self):
+        data = {'to_update':'bio','bio':'This is a test bio'}
+        response = self.client.post(reverse('update_profile'), data=data, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEquals(response.status_code, 200)
+
+    def test_update_profile_all_details(self):
+        data = {'to_update':'all_details','fullname':'Test User','secondary_email':'test@gmail.com',
+                'website':'www.test.com','location':'Test Location','company':'Test Company'}  
+        response = self.client.post(reverse('update_profile'), data=data, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEquals(response.status_code, 200)
+
+    def test_delete_user(self):
+        data = {'username':self.user.username}
+        response = self.client.post(reverse('delete_user'), data=data, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.json(), {'status':'deleted'})
+
+    def test_access_new_book_page_user_not_librarian(self):
+        self.user.groups.add(self.member_group)
+        response = self.client.get(reverse('new_book'))
+        self.assertEquals(response.status_code, 302)
+
+    def test_access_new_book_page_user_librarian(self):
+        self.user.groups.add(self.librarian_group)
+        response = self.client.get(reverse('new_book'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'librarian/new_book.html')
         
     def test_upper_nav_view(self):
         response = self.client.get(reverse('upper-nav'))
