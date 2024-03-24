997c6b3dcddb6611dd593f8185c489c2bdd1f0cd
AlexNdungu <alexmeta517@gmail.com>
2024-03-24 12:50:23 +0300

    Views Testing Process

--- library/tests/test_views.py
+++ library/tests/test_views.py
@@ -47,6 +47,46 @@ class TestViews(TestCase):
         response = client.get(reverse('signin'))
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'Main/signin.html')
+
+    def test_logout(self):
+        response = self.client.get(reverse('signout'))
+        self.assertEquals(response.status_code, 302)
+
+    def test_sign_in_user_user_doesnt_exist(self):
+        client = Client()
+        email = 'usernot@gmail.com'
+        data = {'email':email}
+        response = client.post(reverse('signin_user'), data=data, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEquals(response.json(), {'status':'not_found'})
+
+    def test_sign_in_user_wrong_password(self):
+        client = Client()
+        new_user = User.objects.create_user(username='newuser',password='newpassword',email='newuser@gmail')
+        email = new_user.email
+        wrong_password = 'wrongpassword'
+        data = {'email':email, 'password':wrong_password}
+        response = client.post(reverse('signin_user'), data=data, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEquals(response.json(), {'status':'wrong_password'})
+
+    def test_sign_in_user_correct_password(self):
+        client = Client()
+        new_user = User.objects.create_user(username='newuser',password='newpassword',email='newuser@gmail')
+        email = new_user.email
+        password = 'newpassword'
+        data = {'email':email, 'password':password}
+        response = client.post(reverse('signin_user'), data=data, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEquals(response.json(), {'status':'found'})
+
+    # This is an example for all the views with the wrapper @login_required
+    def test_access_profile_logged_out(self):
+        client = Client()
+        response = client.get(reverse('profile'))
+        self.assertEquals(response.status_code, 302)
+
+    def test_access_profile_logged_in(self):
+        response = self.client.get(reverse('profile'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'Main/profile.html')
         
     def test_upper_nav_view(self):
         response = self.client.get(reverse('upper-nav'))
